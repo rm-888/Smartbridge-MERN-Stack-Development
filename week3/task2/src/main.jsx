49da4ae09894cb64e4c42b3fbae7e04bdd73438a
rm-888 <138863099+rm-888@users.noreply.github.com>
2025-02-15 22:57:02 +0530

    Add files via upload

---
+++ week3/task2/src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import Form from './form.jsx'
+import Header from './Header.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Header />
+    <Form />
+    <App />
+  </StrictMode>,
+)
